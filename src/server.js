11c6fabc86dfefbaf5e3664613c54c0cacd5ca7d
Arnold <arnoldingunza@gmail.com>
2020-09-30 01:39:01 -0500

    actualizaciones para heroku server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = express();
 
 app.use(express.static(__dirname+'/dist/ng-blog'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/registrarHorarioFront/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
